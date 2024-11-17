ef20a4fbe4a9e28061a8759e4b7ad26e807f4ae4
Hussain Zaidi <atiqueulhussainz@gmail.com>
2024-11-17 20:53:09 +0500

    Man.

--- backend/db.js
+++ backend/db.js
@@ -8,7 +8,9 @@ const client = new Client({
   database: 'postgres', // Replace with your Supabase database name
   password: process.env.DB_PW, // Replace with your Supabase password
   port: 5432, // Supabase uses the standard PostgreSQL port
-  ssl:false,
+  ssl: {
+    rejectUnauthorized: false, // Necessary for secure connections to Supabase
+  },
 });
 
 module.exports = client;
