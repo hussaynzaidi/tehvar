c0ea9fa9eec7c371640a5e7c8bd82f792d387d99
Hussain Zaidi <atiqueulhussainz@gmail.com>
2024-11-17 21:55:23 +0500

    Recheck

---
+++ backend/api/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const path = require('path');
+const port = 8000
+const client = require('./db')
+app.use(cors());
+app.use(express.json());
+
+const staffRoutes = require('./routes/staffRoutes');
+const attendeeRoutes = require('./routes/attendeeRoutes');
+const speakerRoutes = require('./routes/speakerRoutes');
+const artistRoutes = require('./routes/artistRoutes');
+const viewRoutes = require('./routes/viewRoutes');
+const sponsorRoutes = require('./routes/sponsorRoutes');
+const loginRoutes = require('./routes/loginRoutes');
+const requireAuth = require('./middleware/requireAuth');
+
+
+// app.use(requireAuth)
+app.use('/api/attendee', attendeeRoutes);
+app.use('/api/speaker', speakerRoutes);
+app.use('/api/artist', artistRoutes);
+app.use('/api/view', viewRoutes);
+app.use('/api/staff', staffRoutes);
+app.use('/api/sponsors', sponsorRoutes);
+app.use('/api/auth', loginRoutes);
+
+app.use((err, req, res, next) => {
+    console.error('Unhandled error:', err);
+    res.status(500).send('Internal Server Error');
+});
+
+app.use('/test', () => {
+    console.log("test")
+    res.send("tested")
+})
+client.connect();
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
