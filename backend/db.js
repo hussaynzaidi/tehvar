16e59f9cb08a4eeb16bb4cec6c67fff8f3171825
Hussain Zaidi <atiqueulhussainz@gmail.com>
2024-11-17 20:42:23 +0500

    Switch to session mode

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const client = new Client({
   host: 'aws-0-ap-southeast-1.pooler.supabase.com', // e.g., db.supabase.co
   database: 'postgres', // Replace with your Supabase database name
   password: process.env.DB_PW, // Replace with your Supabase password
-  port: 6543, // Supabase uses the standard PostgreSQL port
+  port: 5432, // Supabase uses the standard PostgreSQL port
   ssl: {
     rejectUnauthorized: false, // Necessary for secure connections to Supabase
   },
